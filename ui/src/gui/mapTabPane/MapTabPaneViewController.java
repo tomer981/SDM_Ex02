ddb66f0cee55ad85066a1beb832aecb9bab43057
Aviad BD <aviad.bendov@gmail.com>
2020-09-26 18:02:22 +0300

    Base?

---
+++ ui/src/gui/mapTabPane/MapTabPaneViewController.java
@@ -0,0 +1,15 @@
+package gui.mapTabPane;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class MapTabPaneViewController {
+
+
+    public @FXML
+    AnchorPane mapArea;
+
+    public void initialize() {
+
+    }
+}
